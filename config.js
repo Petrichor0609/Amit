b58c5cf422e84a9e734f7e8928c95e7c093a1f67
Petrichor0609 <petrichor530@gmail.com>
2025-09-03 09:51:42 -0700

    Update config.js

--- config.js
+++ config.js
@@ -3,17 +3,25 @@ var config = {
     // 句子的长度可以任意， 你可以写十句话， 二十句话都可以
     // 每句话尽量不要超过15个字,不然展示效果可能不太好
     texts: [
-        "送给我",      //这里,每句话结尾的最后一个逗号必须是英文的哦!! 很重要哦!!
-        "心爱的小可爱",  // 同上...
-        "今天是你的生日",
-        "这是我们在一起的",
-        "第三个生日了哦",
-        "去年的生日",
-        "还记得在王婆串串吃的呢",
-        "今年要吃好的喽哦",
-        "要把我家可爱猪猪喂饱饱",
-        "然后抱走",
-        "YAMI~~",
+        "Hi~ What date is it today?",      //这里,每句话结尾的最后一个逗号必须是英文的哦!! 很重要哦!!
+        "It's your special dayyy!!!🎉",  // 同上...
+        "Happy Birthday to you!!! Happy Birthday to youuuu~🎵",
+        "To the sweetest",
+        "funniest",
+        "amazing guy I met🥳",
+        "See, time flies omg",
+        "You are already 21 now😏!*hey, just 1 year younger than me rn XD",
+        "I'm glad and proud of youuuuu😌!",
+        "So happy our paths crossed and that we get to share a part of our lives TOGETHER :D",
+        "You deserve all the happiness😊",
+        "I hope this year, not enough XD, in your life",
+        "with more smiles, more laughter, and way more good vibes than before",
+        "hahah you know what",
+        "I was actually going to pull a little prank at first",
+        "ummm XD haha well, I changed my mind, but at least looks not that bad right now ",
+        "btw this website is yours now, no one will know it XD",
+        "oops except me hahaha",
+        "Happy birthday again!!!",
     ],
     /**
      * imgs 可以不填, 但是如果要填写的话必须遵循下面的格式
@@ -25,18 +33,20 @@ var config = {
      * Tip: 图片最好用正方形or接近正方形, 看起来效果更好
      */
     imgs: {
-        "心爱的小可爱": "./imgs/xiaokeai.png",
+        "To the sweetest": "./imgs/sweet.jpg",
+        "funniest": "./imgs/funny.jpg",
+        "amazing guy I met🥳": "./imgs/xiaokeai.jpg",
         // "今天是你的生日": "./imgs/birthday.jpg",
     },
     // 按钮文字描述, 以下是默认的按钮文字，英文的，您可以改成你喜欢的文字
     desc: {
-        turn_on: "开始",
-        play: "音乐",
-        bannar_coming: "颜色",
-        balloons_flying: "好像少点东西",
-        cake_fadein: "蛋糕？",
-        light_candle: "蜡烛？",
-        wish_message: "生日快乐",
+        turn_on: "Start",
+        play: "Music",
+        bannar_coming: "Banner",
+        balloons_flying: "umm missing something?",
+        cake_fadein: "Cake?",
+        light_candle: "Light Candle?",
+        wish_message: "Happy Birthday!",
         story: "A MESSAGE FOR YOU",
     }
 };
